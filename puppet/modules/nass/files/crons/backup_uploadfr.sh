290585b4dac54198b24becb859227ad2522e9692
Nassim Ait Boufrad <nassim.aitboufrad@francetv.fr>
2013-10-19 02:54:04 +0200

    avec le bon dossier c'est mieux

---
+++ puppet/modules/nass/files/crons/backup_uploadfr.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Date du jour
+date=`date '+%d-%m-%Y'`
+# Date d'il y a 7 jours
+date2=`date --date '2 days ago' "+%d-%m-%Y"`
+
+tar -czf /space/secure/backup/uploadfr/backup_uploadfr_$date.tar.gz /space/www/uploadfr.com/images
+cd /space/backup/uploadfr
+ln -s -f backup_uploadfr_$date.tar.gz backup_uploadfr.tar.gz
+echo Tar.gz de uploadfr OK
+
+rm -f /space/secure/backup/uploadfr/backup_uploadfr_$date2.tar.gz
+echo Backup de $date2 supprime OK
